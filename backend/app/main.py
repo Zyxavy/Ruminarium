f11c4741b3dd3c0c8b834e2985c4d07ab1695233
Zyxary <kelpselp@gmail.com>
2026-02-07 18:29:35 +0800

    feat: initialize frontend with React, Vite, and TypeScript
    - Added package.json for managing dependencies and scripts.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Developed App.tsx as the main application component with a health check to the backend.
    - Added React logo SVG in assets.
    - Established global styles in index.css.
    - Set up main.tsx for rendering the application.
    - Configured TypeScript with tsconfig.app.json and tsconfig.node.json for frontend and backend respectively.
    - Created vite.config.ts for Vite configuration.

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .database import test_db_connection
+
+app = FastAPI()
+
+#origins that are allowed to talk to this API
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+def health_check():
+    db_status = "Connected" if test_db_connection() else "Disconnected"
+
+    return {"status": "ok", "database" : db_status, "message": "Journal App Backend is running!"}
